570d24a159aa32ec112a95c4311194d62aa3072d
sharan98 <rudreshsharan@gmail.com>
2019-06-20 11:12:03 +0530

    App.js Router and Route

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,6 +4,7 @@ import uuid from 'uuid';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Department from './components/department/Department'
 import Course from './components/course/Course'
+import Syllabus from './components/syllabus/Syllabus'
 import './App.css';
 
 const API = "https://gublze4aq4.execute-api.us-east-2.amazonaws.com/test";
@@ -12,52 +13,26 @@ const API = "https://gublze4aq4.execute-api.us-east-2.amazonaws.com/test";
 export default class App extends React.Component {
   constructor() {
     super()
-
     this.state = {
-      deptNames: [],
-      courseCodes: [],
-      syllabus: '',
-      department: '',
-      courseCode: ''
+
     }
-
   }
-  
-  componentDidMount() {
-    Axios.get(API + "/department")
-    .then(res => {
-      console.log('axios.get.then')
-      console.log(res)
-      this.setState({
-        deptNames: res.data.body
-      })
-    })
-  }
-
-  displayDepts() {
-    var depts = [];
-    this.state.deptNames.forEach(element => {
-      depts.push(<Link to={`/dept/${element}`}><Department name={element} /></Link>)
-    });
-    
-    return depts;
-  }
-
-  displayCourses() {
-    var courses = [];
-    var department = this.state.department;
-    this.state.courseCodes.forEach(element => {
-      courses.push(<Link to={`/${department}/course/${element}`}><Course name={element} /></Link>)
-    })
-    return courses;
-  }
-
-
   render() {
-    return(
+    return (
       <Router>
-        {this.displayDepts()}
-        
+        {/* <Department /> */}
+        <Route exact path="/" render={() => 
+          <Department />
+        }
+        />
+        <Route exact path="/dept/:deptName" render={({match}) =>
+          <Course dept={match.params.deptName} />
+        }
+        />
+        <Route exact path="/:deptName/course/:courseCode" render={({match}) => 
+          <Syllabus courseCode={match.params.courseCode} />
+        }
+        />
       </Router>
     )
   }

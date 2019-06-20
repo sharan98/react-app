d0c17574add747a335106be10cedca6849bf380e
sharan98 <rudreshsharan@gmail.com>
2019-06-20 18:59:56 +0530

    Updates

---
+++ precache-manifest.56a1b655491fe267c44c37ad30556752.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f88f2389dacb046d8707ee8db0e940",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "9ea5483de9a56e0fecf9",
+    "url": "/react-app/static/css/main.61e4a915.chunk.css"
+  },
+  {
+    "revision": "05968dfe77d257df668c",
+    "url": "/react-app/static/js/2.c4a607a4.chunk.js"
+  },
+  {
+    "revision": "9ea5483de9a56e0fecf9",
+    "url": "/react-app/static/js/main.fea8abfa.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  }
+]);
